768fe4cd94c1ae45790715c729bf410bcd2cda0d
MK <fengmk2@gmail.com>
2025-11-19 20:32:29 +0800

    FIXUP

--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -7,6 +7,7 @@ import { Package } from '../index.js'
 const fixtures = path.join(import.meta.dirname, '../__test__/fixtures');
 const smallData = fs.readFileSync(path.join(fixtures, 'a.json'));
 const largeData = fs.readFileSync(path.join(fixtures, 'npm.json'));
+const superLargeData = fs.readFileSync(path.join(fixtures, '@primer/react.json'));
 
 function JSONParse(data: Buffer) {
   // @ts-expect-error ignore the type error
@@ -16,6 +17,10 @@ function JSONParse(data: Buffer) {
 function SimdJSONParse(data: Buffer) {
   return new Package(data).name;
 }
+
+console.log('small package name: %s, %s', JSONParse(smallData), SimdJSONParse(smallData));
+console.log('large package name: %s, %s', JSONParse(largeData), SimdJSONParse(largeData));
+console.log('super large package name: %s, %s', JSONParse(superLargeData), SimdJSONParse(superLargeData));
 
 const b = new Bench();
 
@@ -33,6 +38,14 @@ b.add('JSONParse large data (22MB)', () => {
 
 b.add('SimdJSONParse large data (22MB)', () => {
   SimdJSONParse(largeData);
+});
+
+b.add('JSONParse super large data (89M)', () => {
+  JSONParse(superLargeData);
+});
+
+b.add('SimdJSONParse super large data (89M)', () => {
+  SimdJSONParse(superLargeData);
 });
 
 await b.run();
